9c7597380d1cf760275398bcaddc767787f44235
AnasbinKabeer <hafizapk007@gmail.com>
2026-01-21 14:18:50 +0530

    leader change on static

--- src/app/components/PrintComp.js
+++ src/app/components/PrintComp.js
@@ -15,7 +15,7 @@ function PrintComp({ data, filterMenu }) {
     const category = ["Masjid", "MNC Ground Floor", "MNC First Floor", "MNC Second Floor", "MNC Outside"]
 
 
-    const leaders = ["Midlaj Muhyudheen (4)", "Salim Abdul Rahman (4)", "Muhammed Jouhar (5)","Muhammed Bilal (5)","Muhammed Shahid (5)"        ]
+    const leaders = ["Niyas Noushad (4)", "Asim Abdul Wahab (4)", "Midlaj Muhyudheen (4)","Fahiz Khalid (4)","Salim Abdul Rahman (4)"        ]
 
 
     const cleaningData = data || sampleData
